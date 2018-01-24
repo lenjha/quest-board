925d0a2a1ccb5933f6eda048a3879d241da00f9d
luke bertram and Lena Ha <lena@>
2018-01-24 11:34:19 -0800

    add quest-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { QuestListComponent } from './quest-list/quest-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestListComponent
   ],
   imports: [
     BrowserModule
