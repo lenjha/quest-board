11a38c04b381012cd87293628c367bc2dc0d0278
Lena Ha and luke bertram <lena+luke@>
2018-01-24 14:12:37 -0800

    configure, initialize, and connect database to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { QuestListComponent } from './quest-list/quest-list.component';
 import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+//^ import Firebase
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+//^ export Firebase configurations
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +29,9 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
